0402d53d811c31b88a23a0f42ac9bcbdeb6efdd5
Corvosinner996 <Corvosinner996@gmail.com>
2018-04-22 09:36:07 +0530

    second try

---
+++ page2/page2.js
@@ -0,0 +1,16 @@
+function hover() {
+	var t1 = document.getElementById("logo1").style.backgroundColor = "black" ;
+	
+}
+function hover2() {
+	var t2 = document.getElementById("logo1").style.backgroundColor = white;
+	
+}
+function hover4() {
+	var t3 = document.getElementById("2main");
+	t3.style.Color = white ;
+}
+function hover3() {
+	var t4 = document.getElementById("2main");
+	t4.style.Color = "rgb(27,27,27)";
+}
